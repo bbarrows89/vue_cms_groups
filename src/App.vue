8434d5a9ae0930a68aa4cf27961a1efffd0afca4
bbarrows89 <bryanbarrows@live.com>
2020-06-14 16:35:30 -0700

    update template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <md-toolbar color="primary" class="expanded-toolbar">
+      <span class="branding">
+        <md-button><router-link to="home">{{title}}</router-link></md-button>
+        <md-button><router-link to="home"><md-icon>home</md-icon></router-link></md-button>
+      </span>
+      <md-button v-if='authenticated' v-on:click='logout' id='logout-button'>Logout</md-button>
+      <md-button v-else v-on:click='login' id='login-button'>Login</md-button>
+      <md-menu md-direction="bottom-start">
+        <md-button md-menu-trigger>
+          <md-icon>menu</md-icon>
+        </md-button>
+        <md-menu-content>
+          <md-menu-item @click="$router.safeNavigate($route, 'home')">Home</md-menu-item>
+          <md-menu-item @click="$router.safeNavigate($route, 'read')">Read</md-menu-item>
+        </md-menu-content>
+      </md-menu>
+    </md-toolbar>
+    <div class="router">
+      <router-view/>
+    </div>
   </div>
 </template>
 
