2cec56ba006c4d8d276942388e21a1ff99c634d4
bbarrows89 <bryanbarrows@live.com>
2020-06-14 16:42:57 -0700

    update home page

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,32 @@
 <template>
   <div id="app">
-    <md-toolbar color="primary" class="expanded-toolbar">
-      <span class="branding">
-        <md-button><router-link to="home">{{title}}</router-link></md-button>
-        <md-button><router-link to="home"><md-icon>home</md-icon></router-link></md-button>
-      </span>
-      <md-button v-if='authenticated' v-on:click='logout' id='logout-button'>Logout</md-button>
-      <md-button v-else v-on:click='login' id='login-button'>Login</md-button>
-      <md-menu md-direction="bottom-start">
-        <md-button md-menu-trigger>
-          <md-icon>menu</md-icon>
-        </md-button>
-        <md-menu-content>
-          <md-menu-item @click="$router.safeNavigate($route, 'home')">Home</md-menu-item>
-          <md-menu-item @click="$router.safeNavigate($route, 'read')">Read</md-menu-item>
-        </md-menu-content>
-      </md-menu>
-    </md-toolbar>
-    <div class="router">
-      <router-view/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  data: () => ({
-    title: "Vue Groups",
-    authenticated: true
-  }),
-  methods: {
-    login () {},
-    async logout () {
-    }
-  },
-}
-</script>
-
 <style>
 #app {
-  font-family: 'Ubuntu', sans-serif;
-}
-
-.branding {
-  flex: 1;
-  text-align: left;
-}
-
-h1, h2 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #2c3e50;
 }
 
-.router {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+#nav {
+  padding: 30px;
 }
 
-.content {
-  padding-top: 16px;
-  padding-bottom: 16px;
-  width: 1024px;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
